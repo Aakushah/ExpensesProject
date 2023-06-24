7f3fc492c57f82fe53396cd708def5b2bbbd3ebc
Äakash prasad sah <aakushah2@gmail.com>
2023-06-24 16:21:15 +0530

    final commit in dev1 branch for expense project

--- src/App.js
+++ src/App.js
@@ -3,27 +3,17 @@ import './App.css';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
-const dummyData=[
-  {
-    id:'e1',
-    title:'SCHOOL FEE',
-    amount:250,
-    date:new Date(2021,5,12)
-  },  
-];
+const dummyData=[    ];
+
 const  App= () => {
   const[expenses,setExpense]= useState(dummyData);
-
+  
   const addExpenseHandler = (expense) => {
 
     const updatedExpenses=[expense,...expenses];
     setExpense(updatedExpenses);
 
-
   };
-
-
-
 
 
  return (
